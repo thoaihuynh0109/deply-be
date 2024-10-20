e374dcac64b0e17495cd78d96411d933fa2dd976
mark-huynh-mcc <thoai.huynh@mediconcen.com>
2024-10-21 02:03:24 +0700

    fix 2

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,100 +1,104 @@
-const Category = require('../models/Category.model');
+const Category = require("../models/Category.model");
 
 const CategoryController = {
-    getAll: (req, res) => {
-        Category.find({})
-            .then((categories) => res.status(200).json(categories))
-            .catch(() => res.status(404).json('Không tìm thấy danh sách danh mục.'));
-    },
+  getAll: (req, res) => {
+    Category.find({})
+      .then((categories) => res.status(200).json(categories))
+      .catch(() => res.status(404).json("Không tìm thấy danh sách danh mục."));
+  },
 
-    getParentCategory: (req, res) => {
-        Category.find({ parentId: null })
-            .then((categories) => res.status(200).json(categories))
-            .catch(() => res.status(404).json('Không tìm thấy danh sách danh mục.'));
-    },
+  getParentCategory: (req, res) => {
+    Category.find({ parent: null })
+      .then((categories) => res.status(200).json(categories))
+      .catch(() => res.status(404).json("Không tìm thấy danh sách danh mục."));
+  },
 
-    getChildByPId: (req, res) => {
-        Category.findOne({ _id: req.params.id })
-            .then((category) => {
-                Category.find({ parentId: category._id })
-                    .then((categories) => res.status(200).json(categories))
-                    .catch(() => res.status(404).json('Không tìm thấy danh sách danh con.'));
-            })
-            .catch(() => {
-                res.status(404).json('Không tìm thấy danh mục.');
-            });
-    },
+  getChildByPId: (req, res) => {
+    Category.findOne({ _id: req.params.id })
+      .then((category) => {
+        Category.find({ parent: category._id })
+          .then((categories) => res.status(200).json(categories))
+          .catch(() =>
+            res.status(404).json("Không tìm thấy danh sách danh con.")
+          );
+      })
+      .catch(() => {
+        res.status(404).json("Không tìm thấy danh mục.");
+      });
+  },
 
-    getById: (req, res) => {
-        Category.findOne({ _id: req.params.id })
-            .then((category) => {
-                res.json(category);
-            })
-            .catch(() => {
-                res.status(404).json('Không tìm thấy danh mục.');
-            });
-    },
+  getById: (req, res) => {
+    Category.findOne({ _id: req.params.id })
+      .then((category) => {
+        res.json(category);
+      })
+      .catch(() => {
+        res.status(404).json("Không tìm thấy danh mục.");
+      });
+  },
 
-    create: (req, res) => {
-        let parentId = req.body.parentId;
+  create: (req, res) => {
+    let parentId = req.body.parentId;
 
-        if (!parentId) parentId = null;
-        const category = new Category({
-            ...req.body,
-            parentId: parentId,
-        });
-        try {
-            category.save();
-            res.status(201).json(category);
-        } catch (error) {
-            res.status(500).json('Xảy ra lỗi trong quá trình tạo danh mục.');
-        }
-    },
+    if (!parentId) parentId = null;
+    const category = new Category({
+      ...req.body,
+      parent: parentId,
+    });
+    try {
+      category.save();
+      res.status(201).json(category);
+    } catch (error) {
+      res.status(500).json("Xảy ra lỗi trong quá trình tạo danh mục.");
+    }
+  },
 
-    update: (req, res) => {
-        Category.findOne({ _id: req.params.id })
-            .then((category) => {
-                Category.updateOne({ _id: category._id }, req.body)
-                    .then(() => {
-                        res.status(200).json('Cập nhật danh mục thành công.');
-                    })
-                    .catch((err) => {
-                        res.status(500).json('Có lỗi xảy ra trong quá trình cập nhật danh mục.');
-                    });
-            })
-            .catch(() => {
-                res.status(404).json('Không tìm thấy danh mục.');
-            });
-    },
+  update: (req, res) => {
+    Category.findOne({ _id: req.params.id })
+      .then((category) => {
+        Category.updateOne({ _id: category._id }, req.body)
+          .then(() => {
+            res.status(200).json("Cập nhật danh mục thành công.");
+          })
+          .catch((err) => {
+            res
+              .status(500)
+              .json("Có lỗi xảy ra trong quá trình cập nhật danh mục.");
+          });
+      })
+      .catch(() => {
+        res.status(404).json("Không tìm thấy danh mục.");
+      });
+  },
 
-    delete: (req, res) => {
-        Category.findOne({ _id: req.params.id })
-            .then((category) => {
-                Category.updateOne({ _id: category._id }, req.body)
-                    .then(() => {
-                        res.status(204).json('Xóa danh mục thành công.');
-                    })
-                    .catch((err) => {
-                        res.status(500).json('Có lỗi khi xóa danh mục.');
-                    });
-            })
-            .catch(() => {
-                res.status(404).json('Không tìm thấy danh mục.');
-            });
-    },
+  delete: (req, res) => {
+    Category.findOne({ _id: req.params.id })
+      .then((category) => {
+        Category.updateOne({ _id: category._id }, req.body)
+          .then(() => {
+            res.status(204).json("Xóa danh mục thành công.");
+          })
+          .catch((err) => {
+            res.status(500).json("Có lỗi khi xóa danh mục.");
+          });
+      })
+      .catch(() => {
+        res.status(404).json("Không tìm thấy danh mục.");
+      });
+  },
 
-    destroy: async (req, res) => {
-        try {
-            const result = await Category.deleteOne({ _id: req.params.id });
-            if (result.deletedCount === 0) {
-                res.status(404).json('Không tìm thấy danh mục.');
-            } else {
-                res.status(204).json('Xóa vĩnh viễn danh mục thành công.');
-            }
-        } catch (error) {
-            res.status(500).json('Có lỗi khi xóa danh mục');
-        }
-    },
+  destroy: async (req, res) => {
+    try {
+      const result = await Category.deleteOne({ _id: req.params.id });
+      if (result.deletedCount === 0) {
+        res.status(404).json("Không tìm thấy danh mục.");
+      } else {
+        res.status(204).json("Xóa vĩnh viễn danh mục thành công.");
+      }
+    } catch (error) {
+      res.status(500).json("Có lỗi khi xóa danh mục");
+    }
+  },
 };
 
 module.exports = CategoryController;
--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,102 +1,103 @@
-const Order = require('../models/Order.model');
+const Order = require("../models/Order.model");
 
 const OrderController = {
-    getAll: async (req, res) => {
-        try {
-            const orders = await Order.find({ deleted: false }, null, { lean: true });
-            res.status(200).json(orders);
-        } catch (error) {
-            res.status(404).json({ message: error.message });
-        }
-    },
+  getAll: async (req, res) => {
+    try {
+      const orders = await Order.find({ deleted: false }, null, { lean: true });
+      res.status(200).json(orders);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  },
 
-    getById: async (req, res) => {
-        try {
-            const { id } = req.params;
-            const order = await Order.findOne({ _id: id });
-            if (!order) {
-                return res.status(404).json({ message: 'Order not found' });
-            }
-            res.status(200).json(order);
-        } catch (error) {
-            res.status(404).json({ message: error.message });
-        }
-    },
+  getById: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const order = await Order.findOne({ _id: id });
+      if (!order) {
+        return res.status(404).json({ message: "Order not found" });
+      }
+      res.status(200).json(order);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  },
 
-    getByUserId: async (req, res) => {
-        try {
-            const { userId } = req.params;
-            const orders = await Order.find({ userId }, null, { lean: true });
-            res.status(200).json(orders);
-        } catch (error) {
-            res.status(404).json({ message: error.message });
-        }
-    },
+  getByUserId: async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const orders = await Order.find({ user: userId }, null, { lean: true });
+      res.status(200).json(orders);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  },
 
-    create: async (req, res) => {
-        const { userId, item } = req.body;
-        try {
-            const newOrder = new Order({
-                userId,
-                status: 'IN_SHOPPING_CART',
-            });
-            if (!!item) {
-                const product = await Product.findOne({ _id: item.productId });
-                const variant = await Variant.findOne({ _id: item.variantId });
-                newOrder.item = {
-                    productId: product._id,
-                    productName: product.nameEn,
-                    imagePath: product.imagePath,
-                    size: variant.size,
-                    price: variant.price,
-                    priceSale: variant.priceSale,
-                    quantity: item.quantity,
-                };
-                const totalPrice = (newOrder.item.priceSale ?? newOrder.item.price) * item.quantity;
-                newOrder.totalPrice = totalPrice;
-            }
-            const order = await newOrder.save();
-            res.status(201).json(order);
-        } catch (error) {
-            res.status(404).json({ message: error.message });
-        }
-    },
+  create: async (req, res) => {
+    const { userId, item } = req.body;
+    try {
+      const newOrder = new Order({
+        user: userId,
+        status: "IN_SHOPPING_CART",
+      });
+      if (!!item) {
+        const product = await Product.findOne({ _id: item.productId });
+        const variant = await Variant.findOne({ _id: item.variantId });
+        newOrder.item = {
+          product: product._id,
+          productName: product.nameEn,
+          imagePath: product.imagePath,
+          size: variant.size,
+          price: variant.price,
+          priceSale: variant.priceSale,
+          quantity: item.quantity,
+        };
+        const totalPrice =
+          (newOrder.item.priceSale ?? newOrder.item.price) * item.quantity;
+        newOrder.totalPrice = totalPrice;
+      }
+      const order = await newOrder.save();
+      res.status(201).json(order);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  },
 
-    update: async (req, res) => {
-        const { id } = req.params;
-        const { item } = req.body;
-        try {
-            const order = await Order.findOne({ _id: id });
-            if (!order) {
-                return res.status(404).json({ message: 'Order not found' });
-            }
-            await Order.updateOne(
-                { _id: order._id },
-                {
-                    status: 'IN_SHOPPING_CART',
-                    item,
-                    totalPrice: item.quantity * product.price,
-                },
-            );
-            res.status(200).json({ message: 'Order updated successfully' });
-        } catch (error) {
-            res.status(404).json({ message: error.message });
+  update: async (req, res) => {
+    const { id } = req.params;
+    const { item } = req.body;
+    try {
+      const order = await Order.findOne({ _id: id });
+      if (!order) {
+        return res.status(404).json({ message: "Order not found" });
+      }
+      await Order.updateOne(
+        { _id: order._id },
+        {
+          status: "IN_SHOPPING_CART",
+          item,
+          totalPrice: item.quantity * product.price,
         }
-    },
+      );
+      res.status(200).json({ message: "Order updated successfully" });
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  },
 
-    delete: async (req, res) => {
-        const { id } = req.params;
-        try {
-            const order = await Order.findOne({ _id: id });
-            if (!order) {
-                return res.status(404).json({ message: 'Order not found' });
-            }
-            await Order.updateOne({ _id: id }, { deleted: true });
-            res.status(200).json({ message: 'Order deleted successfully' });
-        } catch (error) {
-            res.status(404).json({ message: error.message });
-        }
-    },
+  delete: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const order = await Order.findOne({ _id: id });
+      if (!order) {
+        return res.status(404).json({ message: "Order not found" });
+      }
+      await Order.updateOne({ _id: id }, { deleted: true });
+      res.status(200).json({ message: "Order deleted successfully" });
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  },
 };
 
 module.exports = OrderController;
