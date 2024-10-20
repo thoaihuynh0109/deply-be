fae10ac8869f8d0860c4722127a8efec1629eff2
mark-huynh-mcc <thoai.huynh@mediconcen.com>
2024-10-21 01:52:11 +0700

    test 2

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-import app from "./src/index.js";
-
-export default app;
-require("./configs/db.config");
 const express = require("express");
-const cors = require("cors");
-const route = require("./routes/index.route");
-const morgan = require("morgan");
-
 const app = express();
-const port = 3001;
+const port = 3000;
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("combined"));
-
-app.use("/", (req, res) => {
-  return "Hello World!";
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
