797e2d13b34ef19552baa655ef5bc7c6224e0540
mark-huynh-mcc <thoai.huynh@mediconcen.com>
2024-10-21 01:56:24 +0700

    fix 1

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,25 +1,24 @@
-const productRouter = require('./product.route');
-const authRouter = require('./auth.route');
-const userRouter = require('./user.route');
-const categoryRouter = require('./category.route');
-const brandRouter = require('./brand.route');
-const orderRouter = require('./order.route');
-const paymentRouter = require('./payment.route');
-const promotionRouter = require('./promotion.route');
+const productRouter = require("./product.route");
+const authRouter = require("./auth.route");
+const userRouter = require("./user.route");
+const categoryRouter = require("./category.route");
+const brandRouter = require("./brand.route");
+const orderRouter = require("./order.route");
+const paymentRouter = require("./payment.route");
+const promotionRouter = require("./promotion.route");
 
 function route(app) {
-    app.use(`/products`, productRouter);
-    app.use('/auths', authRouter);
-    app.use('/users', userRouter);
-    app.use('/categories', categoryRouter);
-    app.use('/brands', brandRouter);
-    app.use('/orders', orderRouter);
-    app.use('/payments', paymentRouter);
-    app.use('/promotions', promotionRouter);
+  app.use(`/products`, productRouter);
+  app.use("/auths", authRouter);
+  app.use("/users", userRouter);
+  app.use("/categories", categoryRouter);
+  app.use("/brands", brandRouter);
+  app.use("/orders", orderRouter);
+  app.use("/payments", paymentRouter);
 
-    app.use('/', () => {
-        return 'Hello World!';
-    });
+  app.get("/", () => {
+    return "Hello World!";
+  });
 }
 
 module.exports = route;
