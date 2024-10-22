8675d169e1163d16be3024c0d4c163d8da299b37
mark-huynh-mcc <thoai.huynh@mediconcen.com>
2024-10-22 19:16:12 +0700

    dep 1

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,24 +1,23 @@
-const authRouter = require('./auth.route');
-const userRouter = require('./user.route');
-const categoryRouter = require('./category.route');
-const brandRouter = require('./brand.route');
-const orderRouter = require('./order.route');
-const paymentRouter = require('./payment.route');
-const promotionRouter = require('./promotion.route');
+const productRouter = require("./product.route");
+const authRouter = require("./auth.route");
+const userRouter = require("./user.route");
+const categoryRouter = require("./category.route");
+const brandRouter = require("./brand.route");
+const orderRouter = require("./order.route");
+const paymentRouter = require("./payment.route");
 
 function route(app) {
-    app.use(`/products`, productRouter);
-    app.use('/auths', authRouter);
-    app.use('/users', userRouter);
-    app.use('/categories', categoryRouter);
-    app.use('/brands', brandRouter);
-    app.use('/orders', orderRouter);
-    app.use('/payments', paymentRouter);
-    // app.use('/promotions', promotionRouter);
+  app.use(`/products`, productRouter);
+  app.use("/auths", authRouter);
+  app.use("/users", userRouter);
+  app.use("/categories", categoryRouter);
+  app.use("/brands", brandRouter);
+  app.use("/orders", orderRouter);
+  app.use("/payments", paymentRouter);
 
-    app.get('/', (req, res) => {
-        res.send('Hello World!');
-    });
+  app.use("/", (req, res) => {
+    res.send("Weather Forecast App!!");
+  });
 }
 
 module.exports = route;
